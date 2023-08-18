d9524f7f98e6b965167b54b1f43a6a7ed6b70e20
David <2018dpollack@gmail.com>
2023-08-17 17:17:22 -0700

    added seed data

--- practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/db/seeds.rb
+++ practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# user1 = User.create!(name: 'CJ')
+#   user2 = User.create!(name: 'Flarnie')
+#   user3 = User.create!(name: 'Jeff')
+#   user4 = User.create!(name: 'Georges St. Pierre')
+#   user5 = User.create!(name: 'Ned')
+
+#   course1 = Course.create!(name: 'Ruby 101', instructor_id: user5.id)
+#   course2 = Course.create!(name: 'Ruby 102', instructor_id: user5.id, prereq_id: course1.id)
+
+#   Enrollment.create!(student_id: user3.id, course_id: course1.id)
+#   Enrollment.create!(student_id: user4.id, course_id: course1.id)
+#   Enrollment.create!(student_id: user1.id, course_id: course2.id)
+#   Enrollment.create!(student_id: user2.id, course_id: course2.id)
+
+david = User.create!(email: "David@gmail.com")
+davis = User.create!(email: "Davis@gmail.com")
+
+su1 = ShortenedUrl.create!(long_url: "testing123",user_id: david.id)
+su2 = ShortenedUrl.create!(long_url: "another_testing123", user_id: davis.id)
+
